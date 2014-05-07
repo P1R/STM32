d1b0b6d6b4265246153498e885f990dc9206c6b4
root <root@BlackFive.lan>
2014-05-07 02:44:45 -0500

    Terminando programa, falta arreglar canvas y ventanas pero por urgencia de entrega se deja asi

--- Practica7/dummies.py
+++ Practica7/dummies.py
@@ -4,8 +4,14 @@ from PyQt4 import QtGui
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
 import matplotlib.pyplot as plt
+import time
+from collections import deque
+import numpy as np
+import sys, serial
+from pylab import pause
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)#timeout=0, rtscts=1)
 
-import random
+from random import randrange
 
 class Window(QtGui.QDialog):
     def __init__(self, parent=None):
@@ -42,10 +48,49 @@ class Window(QtGui.QDialog):
         self.setLayout(layout)
     def Foco(self):
         print 1
+	ser.write("1")
     def Ventilador(self):
         print 2
+	ser.write("2")
+
     def plot(self):
-        ''' plot some random stuff '''
+        	#plot some random stuff
+	#Acumulador
+	a1 = deque([0]*100)
+        # random data
+        d = self.random_gen()
+
+        # create an axis
+        ax = self.figure.add_subplot(111)
+        #ax.axes(xlim=(0, 50), ylim=(0, 10))
+	#grafica continuamente
+	ax.axis((0,100,0,35))
+	line, = ax.plot(a1)
+	for i in range(0,200):
+	    a1.appendleft(next(d))
+	    datatoplot = a1.pop()
+	    line.set_ydata(a1)
+	    self.canvas.draw()
+	    print a1[0]
+	    i += 1
+	    time.sleep(0.1)
+	    pause(0.0001)
+
+    def random_gen(self):
+        while True:
+            line = ser.readline()
+            val = float(line)
+            yield val
+            time.sleep(0.1)
+
+        # discards the old graph
+       # ax.hold(False)
+
+        # plot data
+        #ax.plot(d, '*-')
+''' 
+    def plot(self):
+        	#plot some random stuff
         # random data
         data = [random.random() for i in range(10)]
 
@@ -60,6 +105,7 @@ class Window(QtGui.QDialog):
 
         # refresh canvas
         self.canvas.draw()
+''' 
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
